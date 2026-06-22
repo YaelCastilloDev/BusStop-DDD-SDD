5ade2b7f018c0fbfe0631eac75973bebd79a66f9
Yael Castillo <yael.castillo.dev@gmail.com>
2026-06-21 22:02:28 -0600

    feat: configure Aspire host with PostGIS image, PostgresConnection, and HTTPS port

--- src/BusStop.AspireHost/AppHost.cs
+++ src/BusStop.AspireHost/AppHost.cs
@@ -1,11 +1,14 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
+  .WithImage("postgis/postgis")
+  .WithImageTag("18-3.6")
   .WithLifetime(ContainerLifetime.Persistent);
 
-var busStopDb = postgres.AddDatabase("busstop");
+var busStopDb = postgres.AddDatabase("PostgresConnection");
 
 builder.AddProject<Projects.BusStop_Web>("web")
+  .WithEndpoint("https", e => e.Port = 57679)
   .WithReference(busStopDb)
   .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName)
   .WaitFor(busStopDb);
