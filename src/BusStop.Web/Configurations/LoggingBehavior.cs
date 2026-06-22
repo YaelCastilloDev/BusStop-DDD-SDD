e00e7e82a7d5fd674beb7803afeb48dd01382570
Yael Castillo <yael.castillo.dev@gmail.com>
2026-06-21 22:02:21 -0600

    feat: add LoggingBehavior pipeline behavior and register UseCases assembly

---
+++ src/BusStop.Web/Configurations/LoggingBehavior.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Ardalis.GuardClauses;
+using Mediator;
+
+namespace BusStop.Web.Configurations;
+
+public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger = logger;
+
+    public async ValueTask<TResponse> Handle(
+        TRequest request,
+        MessageHandlerDelegate<TRequest, TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        Guard.Against.Null(request);
+
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation("Handling {RequestName} with {@Request}", typeof(TRequest).Name, request);
+        }
+
+        var sw = Stopwatch.StartNew();
+
+        var response = await next(request, cancellationToken);
+
+        sw.Stop();
+
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation("Handled {RequestName} with {Response} in {ElapsedMilliseconds} ms",
+                typeof(TRequest).Name, response, sw.ElapsedMilliseconds);
+        }
+
+        return response;
+    }
+}
