1492c2791077c47808ec9d15fe818a536f1f4c6c
Yael Castillo <yael.castillo.dev@gmail.com>
2026-07-26 00:03:28 -0600

    style: fix pre-existing formatting issues to unblock pre-commit hook
    - .editorconfig: suppress IDE0161 (file-scoped namespace) for EF Core auto-generated migration files
    - StopConfiguration.cs: alphabetize imports
    - ListRoutesHandler.cs: alphabetize imports
    - GetStopsByRouteHandler.cs: alphabetize imports
    - GlobalUsings.cs: alphabetize imports

--- tests/BusStop.UnitTests/GlobalUsings.cs
+++ tests/BusStop.UnitTests/GlobalUsings.cs
@@ -1,9 +1,9 @@
 ﻿global using System.Runtime.CompilerServices;
 global using Ardalis.GuardClauses;
 global using Ardalis.SharedKernel;
-global using Shouldly;
 global using Mediator;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Logging;
 global using NSubstitute;
+global using Shouldly;
 global using Xunit;
